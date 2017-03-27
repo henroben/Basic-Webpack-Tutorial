f44c6d7fba298759cc587d31f5ea559695df01cd
Ben Mercer <ben@henroben.co.uk>
2017-03-27 12:58:34 +0100

    Simple code splitting

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,15 +1,18 @@
-import big from '../assets/big.jpeg'; // need to import so webpack knows about it
+//import big from '../assets/big.jpeg'; // need to import so webpack knows about it
 import small from '../assets/small.jpeg'; // need to import so webpack knows about it
 import '../styles/image_viewer.css'; // need to import so webpack knows about it
 
-const image = document.createElement('img');
-// image.src = 'http://lorempixel.com/400/400';
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    // image.src = 'http://lorempixel.com/400/400';
+    image.src = small;
 
-document.getElementById('image').appendChild(image);
+    document.getElementById('image').appendChild(image);
+};
 
-const bigImage = document.createElement('img');
-// image.src = 'http://lorempixel.com/400/400';
-bigImage.src = big;
 
-document.getElementById('image').appendChild(bigImage);
+//const bigImage = document.createElement('img');
+//// image.src = 'http://lorempixel.com/400/400';
+//bigImage.src = big;
+//
+//document.getElementById('image').appendChild(bigImage);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import sum from './sum';
-import './image_viewer';
+//import sum from './sum';
+//import './image_viewer';
 
-const total = sum(10, 5);
-console.log(total);
+//const total = sum(10, 5);
+//console.log(total);
+
+// For code splitting tutorial
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+button.onclick = () => {
+    System.import('./image_viewer').then(module => {
+        module.default();
+    });
+};
+
+document.body.appendChild(button);
